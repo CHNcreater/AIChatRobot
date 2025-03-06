13434e52b58dcb0739c23fd91be9d969b910eb11
qiaoyinbo <breakerjoe@163.com>
2025-03-07 00:10:35 +0800

    remove login and add secret for auth

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -1,10 +1,14 @@
 import type { NextAuthConfig } from 'next-auth';
+import crypto from 'crypto';
+
+const secret = crypto.randomBytes(32).toString('hex');
 
 export const authConfig = {
   pages: {
     signIn: '/login',
     newUser: '/',
   },
+  secret: secret,
   providers: [
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
     // while this file is also used in non-Node.js environments
